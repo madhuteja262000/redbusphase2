f2e740ce649fc248bb36ee5d37adf28fcc615572
madhutejaswara reddy <madhutejaswara reddy@madhu>
2023-10-12 17:41:03 +0530

    commit

---
+++ src/test/java/com/redbus/pages/HomePages.java
@@ -0,0 +1,44 @@
+package com.redbus.pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class HomePages {
+	@FindBy(xpath = "//input[@id='src']")
+	WebElement fromLocation;
+	@FindBy(xpath = "//input[@id='dest']")
+	WebElement destination;
+	@FindBy(xpath="//div[@id='onwardCal']")
+	WebElement inputDate;
+	@FindBy(xpath="//span[text()='13']")
+	WebElement selectDate;
+	@FindBy(xpath="//button[@id='search_button']")
+	WebElement search;
+	public HomePages(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+	public void HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	public void gettitle(WebDriver driver)
+	{
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+	public void fromLocation(String locationName) throws InterruptedException {
+		fromLocation.sendKeys(locationName);
+		Thread.sleep(1000);
+	}
+	public void toLocation(String destName) throws InterruptedException {
+		destination.sendKeys(destName);
+		Thread.sleep(1000);
+	}
+	public void selectDate() throws InterruptedException {
+		inputDate.click();
+		Thread.sleep(1000);
+		selectDate.click();
+	}
+	public void search() {
+		search.click();
+	}
+}
---
+++ src/test/java/com/redbus/testpages/HomePageTest.java
@@ -0,0 +1,29 @@
+package com.redbus.testpages;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.redbus.base.BaseTest;
+import com.redbus.pages.HomePages;
+
+public class HomePageTest extends BaseTest{
+	@BeforeTest
+	public void test() {
+
+		openBrowser("Firefox");
+
+	}
+	
+	@Test
+	public void Home_Page_Test() throws InterruptedException 
+	{
+        HomePages homepg = new HomePages(driver);
+		driver.get("https://www.redbus.in/");
+		Thread.sleep(1000);
+		homepg.gettitle(driver);
+		homepg.fromLocation("Pune");
+		homepg.toLocation("Bangalore");
+		homepg.selectDate();
+		homepg.search();
+	}
+
+}
